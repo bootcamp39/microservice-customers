1589207f576273c8444c1e609d5aa13158274313
csanmont <csanmont@emeal.nttdata.com>
2022-12-20 14:37:15 -0500

    update controllers

--- src/main/java/com/nttdata/microservice/bankcustomers/services/impl/PersonServiceImpl.java
+++ src/main/java/com/nttdata/microservice/bankcustomers/services/impl/PersonServiceImpl.java
@@ -1,4 +1,4 @@
-package com.nttdata.microservice.bankcustomers.services;
+package com.nttdata.microservice.bankcustomers.services.impl;
 
 import java.util.Date;
 
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.nttdata.microservice.bankcustomers.collections.PersonCollection;
 import com.nttdata.microservice.bankcustomers.collections.enums.PersonTypeEnum;
 import com.nttdata.microservice.bankcustomers.repository.IPersonRepository;
+import com.nttdata.microservice.bankcustomers.services.IPersonService;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -62,54 +63,5 @@ public class PersonServiceImpl implements IPersonService{
 		} );
 	}
 
-	
-
-	/*@Override
-	public Flux<PersonCollection> getAll() throws Exception {
-		return personRepository.findAll();
-	}*/
-
-	/*@Override
-	public Mono<PersonCollection> getByCode(String code) throws Exception {
-		
-		Mono<PersonCollection> col = personRepository.findAll().filter(x -> x.getCode().equals(code)).next();
-		return col;
-	}*/
-
-	/*@Override
-	public Mono<PersonCollection> save(PersonCollection collection) throws Exception {
-
-		collection.setCode(UUID.randomUUID().toString());
-		collection.setState("1");
-		collection.setCreatedAt(new Date());
-		
-		return personRepository.save(collection);
-		
-	}*/
-
-	/*@Override
-	public Mono<PersonCollection> update(PersonCollection updatedCollection, String code) throws Exception {
-		
-		Mono<PersonCollection> result = this.getByCode(code);
-		PersonCollection resultNew = result.block();
-		resultNew.setEmail(updatedCollection.getEmail());
-		resultNew.setUpdatedAt(new Date());
-		return personRepository.save(resultNew);
-	}*/
-
-	/*@Override
-	public Mono<Void> delete(String code) throws Exception {
-		
-		Mono<PersonCollection> result = this.getByCode(code);
-		PersonCollection resultNew = result.block();
-		
-		return personRepository.delete(resultNew);
-	}*/
-
-	/*@Override
-	public Mono<String> getByType(String code) throws Exception {
-		Mono<String> col = personRepository.findAll().filter(x -> x.getCode().equals(code)).next().map(PersonCollection::getPersonType);
-		return col;
-	}*/
 
 }
